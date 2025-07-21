38bde099dba0563c8c4e94a820f7b6509bd36688
ari <ari@smartinet.co.uk>
2025-07-22 01:24:48 +0400

    auto deploy

--- src/Home.js
+++ src/Home.js
@@ -59,7 +59,7 @@ const Home = () => {
     
     console.log('Scheduling image container expand');
     gsap.to('.fyve-image-container', {
-      width: '10vw',
+      width: '22.5vw',
       duration: 0.8,
       ease: 'power2.out',
       delay: 1,
