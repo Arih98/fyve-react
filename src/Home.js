5329c5b996404b4ff34fde2ec3d2a6ab9f2b187b
ari <ari@smartinet.co.uk>
2025-07-21 21:54:47 +0400

    auto deploy

--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,7 @@ import HomeHeader from './HomeHeader';
 import { gsap } from 'gsap';
 import Player from 'lottie-react';
 import './Home.css';
+import React, { useEffect, useState, useRef } from 'react'; // Add useRef here
 
 const Home = () => {
   const navigate = useNavigate();
