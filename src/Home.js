93e6a70c9149da6aaa57273676e155b94a442b75
ari <ari@smartinet.co.uk>
2025-07-21 22:24:15 +0400

    auto deploy

--- src/Home.js
+++ src/Home.js
@@ -5,17 +5,23 @@ import './Home.css';
 const Home = () => {
   useEffect(() => {
     gsap.set('.fyve-mask', { visibility: 'visible' });
-    gsap.fromTo(
+    const tl = gsap.timeline();
+    tl.fromTo(
       '.fyve-text',
       { y: '100%' },
       { y: 0, duration: 0.8, ease: 'power2.out' }
     );
+    tl.to('.fy', { x: -100, duration: 0.5, ease: 'power1.inOut' });
+    tl.to('.ve', { x: 100, duration: 0.5, ease: 'power1.inOut' }, '<');
   }, []);
 
   return (
     <div className="home-page">
       <div className="fyve-mask">
-        <div className="fyve-text">FYVE</div>
+        <div className="fyve-text">
+          <span className="fy">FY</span>
+          <span className="ve">VE</span>
+        </div>
       </div>
     </div>
   );
