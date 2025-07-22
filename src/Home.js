3e8f7c9f017ca93e400580e96dbe7a1481788c89
ari <ari@smartinet.co.uk>
2025-07-22 22:38:06 +0400

    auto deploy

--- src/Home.js
+++ src/Home.js
@@ -30,7 +30,6 @@ const Home = () => {
     if (imageEl) console.log('image rect', imageEl.getBoundingClientRect());
 
     if (hasAnimated) {
-      console.log('Setting final states without animation');
       gsap.set('.fyve-mask', { visibility: 'visible' });
       gsap.set('.fyve-image', { visibility: 'visible' });
       gsap.set('.mask-left', { x: '-100%', transformOrigin: 'left center' });
@@ -41,96 +40,65 @@ const Home = () => {
       gsap.set('.fyve-image-container', { width: '100vw', height: '100vh' });
       gsap.set('.mobile-header', { opacity: 1 });
     } else {
-      console.log('Setting fyve-mask visibility');
       gsap.set('.fyve-mask', { visibility: 'visible' });
-      
-      console.log('Setting initial mask positions');
       gsap.set('.mask-left', { x: '0%', transformOrigin: 'left center' });
       gsap.set('.mask-right', { x: '0%', transformOrigin: 'right center' });
       gsap.set('.fyve-image', { visibility: 'visible' });
       
-      console.log('Starting letter reveal animation');
       gsap.fromTo(
         '.fyve-letter',
         { y: '100%' },
-        { 
-          y: 0, 
-          duration: 0.8, 
-          ease: 'expo.inOut',
-          onStart: () => console.log('Letter reveal animation started'),
-          onComplete: () => console.log('Letter reveal animation completed')
-        }
+        { y: 0, duration: 0.8, ease: 'expo.inOut' }
       );
       
-      console.log('Scheduling FY shift animation');
       gsap.to('.fyve-text:first-child', {
         x: '-0.3vw',
         duration: 0.8,
         ease: 'expo.inOut',
-        delay: 1.2,
-        onStart: () => console.log('FY shift animation started'),
-        onComplete: () => console.log('FY shift animation completed')
+        delay: 1.2
       });
       
-      console.log('Scheduling VE shift animation');
       gsap.to('.fyve-text:last-child', {
         x: '0.3vw',
         duration: 0.8,
         ease: 'expo.inOut',
-        delay: 1.2,
-        onStart: () => console.log('VE shift animation started'),
-        onComplete: () => console.log('VE shift animation completed')
+        delay: 1.2
       });
       
-      console.log('Scheduling image container expand');
       gsap.to('.fyve-image-container', {
         width: '14vw',
         duration: 0.8,
         ease: 'expo.inOut',
-        delay: 1,
-        onStart: () => console.log('Image container expand started'),
-        onComplete: () => console.log('Image container expand completed')
+        delay: 1
       });
       
-      console.log('Scheduling image mask reveal animation');
       gsap.to('.mask-left', {
         x: '-100%',
         duration: 0.8,
         ease: 'expo.inOut',
-        delay: 1,
-        onStart: () => console.log('Image mask left animation started'),
-        onComplete: () => console.log('Image mask left animation completed')
+        delay: 1
       });
       gsap.to('.mask-right', {
         x: '100%',
         duration: 0.8,
         ease: 'expo.inOut',
-        delay: 1,
-        onStart: () => console.log('Image mask right animation started'),
-        onComplete: () => console.log('Image mask right animation completed')
+        delay: 1
       });
 
-      console.log('Scheduling FY slide off left');
       gsap.to('.fyve-text:first-child', {
         x: '-100vw',
         duration: 0.8,
         ease: 'expo.inOut',
-        delay: 2,
-        onStart: () => console.log('FY slide off left started'),
-        onComplete: () => console.log('FY slide off left completed')
+        delay: 2
       });
 
-      console.log('Scheduling VE slide off right');
       gsap.to('.fyve-text:last-child', {
         x: '100vw',
         duration: 0.8,
         ease: 'expo.inOut',
-        delay: 2,
-        onStart: () => console.log('VE slide off right started'),
-        onComplete: () => console.log('VE slide off right completed')
+        delay: 2
       });
 
-      console.log('Scheduling image grow to full screen');
       gsap.to('.fyve-image-container', {
         width: '100vw',
         height: '100vh',
@@ -145,29 +113,29 @@ const Home = () => {
             top: 0,
             width: '100vw',
             height: '100vh',
-            alignItems: 'flex-start'
+            display: 'block'
           });
+          gsap.set('.fyve-text', { display: 'none' });
           gsap.set('.fyve-image-container', {
             clearProps: 'transform,transformOrigin',
+            position: 'absolute',
             top: 0,
             left: 0,
+            width: '100vw',
+            height: '100vh',
             x: 0,
-            y: 0,
+            y: 0
           });
         }
       });
 
-      console.log('Setting initial header opacity');
       gsap.set('.mobile-header', { opacity: 0 });
 
-      console.log('Scheduling header fade in');
       gsap.to('.mobile-header', {
         opacity: 1,
         duration: 0.5,
         ease: 'expo.inOut',
-        delay: 2.8,
-        onStart: () => console.log('Header fade in started'),
-        onComplete: () => console.log('Header fade in completed')
+        delay: 2.8
       });
     }
 
