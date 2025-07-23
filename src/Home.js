9262135f415f5b27afd3daedd46768358bbfae97
ari <ari@smartinet.co.uk>
2025-07-23 08:36:23 +0400

    auto deploy

--- src/Home.js
+++ src/Home.js
@@ -9,6 +9,7 @@ let hasAnimated = false;
 const Home = () => {
   useEffect(() => {
     console.log('Home component mounted');
+    
     const image = document.querySelector('.fyve-image');
     if (image) {
       console.log('Image element found:', image);
@@ -18,6 +19,7 @@ const Home = () => {
     } else {
       console.error('Image element not found');
     }
+
     if (hasAnimated) {
       gsap.set('.fyve-mask', { visibility: 'visible' });
       gsap.set('.fyve-image', { visibility: 'visible' });
@@ -39,62 +41,139 @@ const Home = () => {
       gsap.fromTo(
         '.fyve-letter',
         { y: '100%' },
-        { y: 0, duration: 0.8, ease: 'expo.inOut' }
+        { 
+          y: 0, 
+          duration: 0.8, 
+          ease: 'expo.inOut',
+          onStart: () => console.log('Letter reveal animation started'),
+          onComplete: () => console.log('Letter reveal animation completed')
+        }
       );
-      gsap.to('.fyve-text:first-child', { x: '-0.3vw', duration: 0.8, ease: 'expo.inOut', delay: 1.2 });
-      gsap.to('.fyve-text:last-child', { x: '0.3vw', duration: 0.8, ease: 'expo.inOut', delay: 1.2 });
-      gsap.to('.fyve-image-container', { width: '14vw', duration: 0.8, ease: 'expo.inOut', delay: 1 });
-      gsap.to('.mask-left', { x: '-100%', duration: 0.8, ease: 'expo.inOut', delay: 1 });
-      gsap.to('.mask-right', { x: '100%', duration: 0.8, ease: 'expo.inOut', delay: 1 });
-      gsap.to('.fyve-text:first-child', { x: '-100vw', duration: 0.8, ease: 'expo.inOut', delay: 2 });
-      gsap.to('.fyve-text:last-child', { x: '100vw', duration: 0.8, ease: 'expo.inOut', delay: 2 });
-      gsap.to('.fyve-image-container', { width: '100vw', height: '100vh', duration: 0.8, ease: 'expo.inOut', delay: 2 });
+      gsap.to('.fyve-text:first-child', {
+        x: '-0.3vw',
+        duration: 0.8,
+        ease: 'expo.inOut',
+        delay: 1.2,
+        onStart: () => console.log('FY shift animation started'),
+        onComplete: () => console.log('FY shift animation completed')
+      });
+      gsap.to('.fyve-text:last-child', {
+        x: '0.3vw',
+        duration: 0.8,
+        ease: 'expo.inOut',
+        delay: 1.2,
+        onStart: () => console.log('VE shift animation started'),
+        onComplete: () => console.log('VE shift animation completed')
+      });
+      gsap.to('.fyve-image-container', {
+        width: '14vw',
+        duration: 0.8,
+        ease: 'expo.inOut',
+        delay: 1,
+        onStart: () => console.log('Image container expand started'),
+        onComplete: () => console.log('Image container expand completed')
+      });
+      gsap.to('.mask-left', {
+        x: '-100%',
+        duration: 0.8,
+        ease: 'expo.inOut',
+        delay: 1,
+        onStart: () => console.log('Image mask left animation started'),
+        onComplete: () => console.log('Image mask left animation completed')
+      });
+      gsap.to('.mask-right', {
+        x: '100%',
+        duration: 0.8,
+        ease: 'expo.inOut',
+        delay: 1,
+        onStart: () => console.log('Image mask right animation started'),
+        onComplete: () => console.log('Image mask right animation completed')
+      });
+      gsap.to('.fyve-text:first-child', {
+        x: '-100vw',
+        duration: 0.8,
+        ease: 'expo.inOut',
+        delay: 2,
+        onStart: () => console.log('FY slide off left started'),
+        onComplete: () => console.log('FY slide off left completed')
+      });
+      gsap.to('.fyve-text:last-child', {
+        x: '100vw',
+        duration: 0.8,
+        ease: 'expo.inOut',
+        delay: 2,
+        onStart: () => console.log('VE slide off right started'),
+        onComplete: () => console.log('VE slide off right completed')
+      });
+      gsap.to('.fyve-image-container', {
+        width: '100vw',
+        height: '100vh',
+        duration: 0.8,
+        ease: 'expo.inOut',
+        delay: 2,
+        onStart: () => console.log('Image grow started'),
+        onComplete: () => console.log('Image grow completed')
+      });
       gsap.set('.mobile-header', { opacity: 0 });
-      gsap.to('.mobile-header', { opacity: 1, duration: 0.5, ease: 'expo.inOut', delay: 2.8 });
+      gsap.to('.mobile-header', {
+        opacity: 1,
+        duration: 0.5,
+        ease: 'expo.inOut',
+        delay: 2.8,
+        onStart: () => console.log('Header fade in started'),
+        onComplete: () => console.log('Header fade in completed')
+      });
       gsap.set('.london-mask', { visibility: 'visible' });
       gsap.set('.london-mask .london-text:first-child', { x: '0%', transformOrigin: 'left center' });
       gsap.set('.london-mask .london-text:last-child', { x: '0%', transformOrigin: 'right center' });
       gsap.fromTo(
         '.london-letter',
         { y: '100%' },
-        { y: 0, duration: 0.8, ease: 'expo.inOut' }
+        { y: 0, duration: 0.8, ease: 'expo.inOut', delay: 0 }
       );
       gsap.to('.london-mask .london-text:first-child', { x: '-8vw', duration: 0.8, ease: 'expo.inOut', delay: 1 });
       gsap.to('.london-mask .london-text:last-child', { x: '8vw', duration: 0.8, ease: 'expo.inOut', delay: 1 });
       gsap.to('.london-mask .london-text:first-child', { x: '-100vw', duration: 0.8, ease: 'expo.inOut', delay: 2 });
       gsap.to('.london-mask .london-text:last-child', { x: '100vw', duration: 0.8, ease: 'expo.inOut', delay: 2 });
     }
+
     hasAnimated = true;
   }, []);
 
   return (
     <div className="home-page">
       <HomeHeader />
-      <div className="fyve-wrapper">
-        <div className="fyve-mask">
-          <div className="fyve-text">
-            {'FY'.split('').map((l, i) => <span key={i} className="fyve-letter">{l}</span>)}
-          </div>
-          <div className="fyve-image-container">
-            <img
-              src="/api/Uploads/LOOK-2_137-e1743957431674.webp"
-              alt="Reveal Image"
-              className="fyve-image"
-            />
-            <div className="mask-left"></div>
-            <div className="mask-right"></div>
-          </div>
-          <div className="fyve-text">
-            {'VE'.split('').map((l, i) => <span key={i+2} className="fyve-letter">{l}</span>)}
-          </div>
+      <div className="fyve-mask">
+        <div className="fyve-text">
+          {'FY'.split('').map((letter, index) => (
+            <span key={index} className="fyve-letter">{letter}</span>
+          ))}
         </div>
-        <div className="london-mask">
-          <div className="london-text">
-            {'LON'.split('').map((l, i) => <span key={i} className="london-letter">{l}</span>)}
-          </div>
-          <div className="london-text">
-            {'DON'.split('').map((l, i) => <span key={i+3} className="london-letter">{l}</span>)}
-          </div>
+        <div className="fyve-image-container">
+          <img
+            src="/api/Uploads/LOOK-2_137-e1743957431674.webp"
+            alt="Reveal Image"
+            className="fyve-image"
+          />
+          <div className="mask-left"></div>
+          <div className="mask-right"></div>
+        </div>
+        <div className="fyve-text">
+          {'VE'.split('').map((letter, index) => (
+            <span key={index + 2} className="fyve-letter">{letter}</span>
+          ))}
+        </div>
+      </div>
+      <div className="london-mask">
+        <div className="london-text">
+          {'LON'.split('').map((letter, index) => (
+            <span key={index} className="london-letter">{letter}</span>
+          ))}
+        </div>
+        <div className="london-text">
+          {'DON'.split('').map((letter, index) => (
+            <span key={index + 3} className="london-letter">{letter}</span>
+          ))}
         </div>
       </div>
     </div>
