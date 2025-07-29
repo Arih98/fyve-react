bf2dec7df173d6a0928a20d721c3f6c6e7606190
ari <ari@smartinet.co.uk>
2025-07-30 02:20:07 +0400

    auto deploy

--- src/Home.js
+++ src/Home.js
@@ -106,8 +106,8 @@ const Home = () => {
       const fyveTextY = -1.11;
       const londonX = 1.3;
       const londonY = isMobile ? -14.41 : -1.41;
-      const fyveMoveX = isMobile ? '-185px' : '-0.4vw';
-      const fyveMoveXEnd = isMobile ? '185px' : '0.4vw';
+      const fyveMoveX = isMobile ? '-95px' : '-0.4vw';
+      const fyveMoveXEnd = isMobile ? '95px' : '0.4vw';
       const londonMoveX = isMobile ? '-70px' : '-8.9vw';
       const londonMoveXEnd = isMobile ? '70px' : '8.9vw';
 
