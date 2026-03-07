94228b7e65441c885cb39184383eb680f3c04c21
ari <ari@smartinet.co.uk>
2026-03-08 00:24:12 +0400

    force correct deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+set -e
 git add .
-git commit -m "${1:-Update}"
+git commit -m "${1:-Update}" || true
 git push
 npm run build
-scp -i ~/.ssh/siteground_dev_key -P 18765 -r build/* u2499-f5szapuuzspy@ssh.fyvelondon.com:~/www/dev.fyvelondon.com/public_html/
+ssh -i ~/.ssh/siteground_key -p 18765 u2499-f5szapuuzspy@ssh.fyvelondon.com "rm -rf ~/www/dev.fyvelondon.com/public_html/*"
+scp -i ~/.ssh/siteground_key -P 18765 -r build/* u2499-f5szapuuzspy@ssh.fyvelondon.com:~/www/dev.fyvelondon.com/public_html/
