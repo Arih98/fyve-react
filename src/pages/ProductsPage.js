02826d90d3d20bda237962ffda2e8a69881c402d
ari <ari@smartinet.co.uk>
2026-03-10 03:20:14 +0400

    update

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -30,7 +30,7 @@ const ProductsPage = () => {
     title: product.title || product.name,
     image: product.thumbnail,
     rawPrice: product.price,
-    price: Number(product.price?.current ?? product.price ?? 0)
+    price: product.price?.current ?? 0
   }));
 
   const handleProductClick = (item, e) => {
