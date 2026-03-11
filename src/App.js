c82af3db0172fd4f1716f3fafc5467ab0534e2bc
ari <ari@smartinet.co.uk>
2026-03-11 06:50:31 +0400

    update

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ const StableOutlet = () => {
 
 const Layout = () => {
   const location = useLocation();
+  const isSharedTransition = location.state?.fromProductGrid;
   const showHeader = location.pathname !== '/' && location.pathname !== '/admin';
   const showCart = location.pathname !== '/admin';
   const containerRef = useRef(null);
@@ -68,10 +69,11 @@ const Layout = () => {
       <LayoutGroup>
         <AnimatePresence initial={false}>
           <motion.div
+<motion.div
   key={location.pathname}
   initial={false}
   animate={{ opacity: 1, y: 0 }}
-  exit={{ opacity: 0, y: 0 }}
+  exit={isSharedTransition ? { opacity: 1 } : { opacity: 0 }}
   transition={{ duration: 0.18 }}
   style={{ position: 'absolute', top: 0, left: 0, width: '100%' }}
 >
