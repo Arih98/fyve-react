2884cd45ac634010b185303cb2e596a4972a30dc
ari <ari@smartinet.co.uk>
2026-03-12 15:55:47 +0400

    update

--- src/Header.jsx
+++ src/Header.jsx
@@ -161,27 +161,27 @@ const handleToggleMenu = () => {
     <div className={`mobile-header first-header${isProductDetailPage ? ' pdp-mobile-header' : ''}${hideHeader ? ' hide-header' : ''}${isMenuOpen ? ' menu-active' : ''}${isMenuOpen ? ' menu-open' : ''}`}>
   {BurgerIcon}
 
-  {!isProductDetailPage && (
-    <>
-      <div className="header-logo mobile-hide-logo">
-        <img src="/assets/FYVE-Dark-Logo.png" alt="FYVE White Logo" onClick={() => navigate('/')} />
-      </div>
+  {(!isProductDetailPage || !isMobile) && (
+  <>
+    <div className="header-logo mobile-hide-logo">
+      <img src="/assets/FYVE-Dark-Logo.png" alt="FYVE White Logo" onClick={() => navigate('/')} />
+    </div>
 
-      <div className="mobile-nav-icons">
-        <button className="mobile-nav-icon" onClick={toggleSearch}>
-          <img src="/assets/SearchIcon.svg" alt="Search" />
-        </button>
+    <div className="mobile-nav-icons">
+      <button className="mobile-nav-icon" onClick={toggleSearch}>
+        <img src="/assets/SearchIcon.svg" alt="Search" />
+      </button>
 
-        <button className="mobile-nav-icon" onClick={() => navigate('/account')}>
-          <img src="/assets/AccountIcon.svg" alt="Account" />
-        </button>
+      <button className="mobile-nav-icon" onClick={() => navigate('/account')}>
+        <img src="/assets/AccountIcon.svg" alt="Account" />
+      </button>
 
-        <button className="mobile-nav-icon" onClick={() => navigate('/cart')}>
-          <img src="/assets/BagIcon.svg" alt="Bag" />
-        </button>
-      </div>
-    </>
-  )}
+      <button className="mobile-nav-icon" onClick={() => navigate('/cart')}>
+        <img src="/assets/BagIcon.svg" alt="Bag" />
+      </button>
+    </div>
+  </>
+)}
 
 {isMobile && isProductDetailPage && !isMenuOpen && (
   <button
