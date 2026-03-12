4310f9bfac2b1fa8625927fec621e9cb971bf771
ari <ari@smartinet.co.uk>
2026-03-13 03:19:23 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,116 +1,49 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Routes, Route, useLocation, useOutlet } from 'react-router-dom';
-import Home from './Home';
-import ProductsPage from './pages/ProductsPage';
-import ProductDetail from './ProductDetail';
-import Header from './Header';
-import MobileTopHeader from './MobileTopHeader';
-import Admin from './Admin';
-import Cart from './Cart';
-import CategoryProducts from './CategoryProducts';
-import Checkout from './Checkout';
-import Account from './Account';
-import { MenuContext } from './MenuContext';
-import { CartProvider } from './CartContext';
-import { AnimatePresence, LayoutGroup, motion } from 'framer-motion';
-import './App.css';
-import './Header.css';
-import './HomeHeader.css';
-import ScrollManager from './components/ScrollManager';
+import { useEffect } from 'react';
+import { useLocation, useNavigationType } from 'react-router-dom';
 
-const ProductDetailWrapper = () => {
+const scrollPositions = new Map();
+
+export default function ScrollManager() {
   const location = useLocation();
-  return <ProductDetail key={location.key} />;
-};
-
-const StableOutlet = () => {
-  const o = useOutlet();
-  const [outlet] = useState(o);
-  return outlet;
-};
-
-const Layout = () => {
-  const location = useLocation();
-  const showHeader = location.pathname !== '/' && location.pathname !== '/admin';
-  const showMobileTopHeader = location.pathname !== '/' && location.pathname !== '/admin';
-  const showCart = location.pathname !== '/admin';
-  const containerRef = useRef(null);
+  const navigationType = useNavigationType();
 
   useEffect(() => {
-    const updateHeight = () => {
-      if (containerRef.current) {
-        const contentHeight = containerRef.current.firstElementChild?.scrollHeight || 0;
-        containerRef.current.style.height = `${contentHeight}px`;
+    if ('scrollRestoration' in window.history) {
+      window.history.scrollRestoration = 'manual';
+    }
+  }, []);
+
+  useEffect(() => {
+    const pageKey = `${location.pathname}${location.search}`;
+
+    const saveScroll = () => {
+      scrollPositions.set(pageKey, window.scrollY || 0);
+    };
+
+    window.addEventListener('scroll', saveScroll, { passive: true });
+
+    return () => {
+      saveScroll();
+      window.removeEventListener('scroll', saveScroll);
+    };
+  }, [location.pathname, location.search]);
+
+  useEffect(() => {
+    const pageKey = `${location.pathname}${location.search}`;
+
+    const restoreScroll = () => {
+      if (navigationType === 'POP') {
+        const savedY = scrollPositions.get(pageKey) ?? 0;
+        window.scrollTo(0, savedY);
+      } else {
+        window.scrollTo(0, 0);
       }
     };
 
-    updateHeight();
-    window.addEventListener('resize', updateHeight);
-    const interval = setInterval(updateHeight, 100);
+    requestAnimationFrame(() => {
+      requestAnimationFrame(restoreScroll);
+    });
+  }, [location.pathname, location.search, navigationType]);
 
-    return () => {
-      window.removeEventListener('resize', updateHeight);
-      clearInterval(interval);
-    };
-  }, []);
-
-  return (
-    <div className="App" ref={containerRef} style={{ position: 'relative' }}>
-      {showMobileTopHeader && <MobileTopHeader />}
-      {showHeader && <Header />}
-      {showCart && <Cart />}
-      <LayoutGroup>
-        <AnimatePresence initial={false}>
-          <motion.div
-            key={location.pathname}
-            initial={false}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: 0 }}
-            transition={{ duration: 0.3 }}
-            style={{ position: 'absolute', top: 0, left: 0, width: '100%' }}
-          >
-            <StableOutlet />
-          </motion.div>
-        </AnimatePresence>
-      </LayoutGroup>
-    </div>
-  );
-};
-
-function AppContent() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  useEffect(() => {
-    const handlePopState = () => {
-      console.log('Browser history popstate event triggered (back or forward)');
-    };
-
-    window.addEventListener('popstate', handlePopState);
-    return () => window.removeEventListener('popstate', handlePopState);
-  }, []);
-
-  return (
-    <MenuContext.Provider value={{ isMenuOpen, setIsMenuOpen }}>
-      <CartProvider>
-        <ScrollManager />
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/admin" element={<Admin />} />
-            <Route path="/products" element={<ProductsPage />} />
-            <Route path="/product/:id" element={<ProductDetailWrapper />} />
-            <Route path="/product-category/:slug" element={<CategoryProducts />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/account" element={<Account />} />
-          </Route>
-        </Routes>
-      </CartProvider>
-    </MenuContext.Provider>
-  );
+  return null;
 }
-
-const App = () => {
-  return <AppContent />;
-};
-
-export default App;
