1815ec4feb1c0b91890ebebc84ab8979f45af60b
ari <ari@smartinet.co.uk>
2026-03-23 10:51:05 +0400

    update

--- src/Home.js
+++ src/Home.js
@@ -192,8 +192,8 @@ gsap.to('.home-mobile-top-logo', {
         gsap.fromTo('.london-letter', { y: '100%' }, { y: 0, duration: 1.3 * speed, ease: 'expo.inOut', delay: 0.35 * speed });
         gsap.to('.london-mask .london-text:first-child', { x: londonMoveX, duration: 0.8 * speed, ease: 'expo.inOut', delay: 1.2 * speed });
 gsap.to('.london-mask .london-text:last-child', { x: londonMoveXEnd, duration: 0.8 * speed, ease: 'expo.inOut', delay: 1.2 * speed });
-        gsap.to('.london-mask .london-text:first-child', { x: '-130vw', duration: 0.95 * speed, ease: 'power2.inOut', delay: 2 * speed, onComplete: () => gsap.set('.london-mask .london-text:first-child', { visibility: 'hidden' }) });
-gsap.to('.london-mask .london-text:last-child', { x: '130vw', duration: 0.95 * speed, ease: 'power2.inOut', delay: 2 * speed, onComplete: () => gsap.set('.london-mask .london-text:last-child', { visibility: 'hidden' }) });
+        gsap.to('.london-mask .london-text:first-child', { x: '-140vw', duration: 0.95 * speed, ease: 'power2.inOut', delay: 2 * speed, onComplete: () => gsap.set('.london-mask .london-text:first-child', { visibility: 'hidden' }) });
+gsap.to('.london-mask .london-text:last-child', { x: '140vw', duration: 0.95 * speed, ease: 'power2.inOut', delay: 2 * speed, onComplete: () => gsap.set('.london-mask .london-text:last-child', { visibility: 'hidden' }) });
 
         gsap.to('.lottie-container', {
   autoAlpha: 1,
