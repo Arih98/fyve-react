2abf1005c4ccd2b53d994f919d437c6c22946713
ari <ari@smartinet.co.uk>
2026-03-24 20:38:32 +0400

    update

--- src/App.js
+++ src/App.js
@@ -36,19 +36,21 @@ const Layout = () => {
       {showMobileTopHeader && <MobileTopHeader />}
       {showHeader && <Header />}
       {showCart && <Cart />}
-      <LayoutGroup>
-        <AnimatePresence initial={false} mode="wait">
-          <motion.div
-            key={location.pathname + location.search}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.3 }}
-          >
-            <Outlet />
-          </motion.div>
-        </AnimatePresence>
-      </LayoutGroup>
+      <div className="site-content">
+        <LayoutGroup>
+          <AnimatePresence initial={false} mode="wait">
+            <motion.div
+              key={location.pathname + location.search}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.3 }}
+            >
+              <Outlet />
+            </motion.div>
+          </AnimatePresence>
+        </LayoutGroup>
+      </div>
     </div>
   );
 };
