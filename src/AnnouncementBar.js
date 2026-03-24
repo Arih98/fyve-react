f63c43af73abf9841336fdd74bdd840fd29b3fb3
ari <ari@smartinet.co.uk>
2026-03-24 20:17:44 +0400

    update

--- src/AnnouncementBar.js
+++ src/AnnouncementBar.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import './AnnouncementBar.css';
 
+const items = [
+  'FREE SHIPPING SITEWIDE',
+  'FREE SHIPPING SITEWIDE',
+  'FREE SHIPPING SITEWIDE',
+  'FREE SHIPPING SITEWIDE'
+];
+
 const AnnouncementBar = () => {
   return (
     <div className="announcement-bar">
-      <div className="announcement-track">
-        <span>FREE SHIPPING SITEWIDE</span>
-        <span>FREE SHIPPING SITEWIDE</span>
-        <span>FREE SHIPPING SITEWIDE</span>
-        <span>FREE SHIPPING SITEWIDE</span>
+      <div className="announcement-marquee">
+        <div className="announcement-group">
+          {items.map((item, index) => (
+            <span key={`first-${index}`}>{item}</span>
+          ))}
+        </div>
+        <div className="announcement-group" aria-hidden="true">
+          {items.map((item, index) => (
+            <span key={`second-${index}`}>{item}</span>
+          ))}
+        </div>
       </div>
     </div>
   );
