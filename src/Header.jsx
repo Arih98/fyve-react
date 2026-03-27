c98e3025c0cb69be22fdf0038045ad8f71374bc8
ari <ari@smartinet.co.uk>
2026-03-27 15:49:05 +0400

    update

--- src/Header.jsx
+++ src/Header.jsx
@@ -134,7 +134,7 @@ useEffect(() => {
 
     const timeout = setTimeout(() => {
       setDelayTransparentHeader(false);
-    }, 500);
+    }, 250);
 
     prevMenuStateRef.current = menuState;
     return () => clearTimeout(timeout);
