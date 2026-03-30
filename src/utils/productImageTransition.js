644c0b80e5fcc06bc71f01f8f2c42707c0e42e70
ari <ari@smartinet.co.uk>
2026-03-30 16:11:43 +0400

    update

--- src/utils/productImageTransition.js
+++ src/utils/productImageTransition.js
@@ -122,7 +122,7 @@ const createClone = ({ src, fromRect, fromStyle, zIndex }) => {
   clone.style.transform = 'scale(1.18)';
   clone.style.pointerEvents = 'none';
   clone.style.zIndex = String(zIndex);
-  clone.style.background = fromStyle.backgroundColor || '#f7f7f7';
+  clone.style.background = fromStyle.backgroundColor || 'transparent';
   clone.style.borderRadius = fromStyle.borderRadius || '0px';
   clone.style.boxSizing = fromStyle.boxSizing || 'border-box';
   clone.style.transformOrigin = 'center center';
