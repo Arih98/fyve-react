0665e15fbdbb71d84b2cfb3d1cab63c7ad5af37c
ari <ari@smartinet.co.uk>
2026-04-12 10:52:22 +0100

    update

--- src/components/ScrollManager.jsx
+++ src/components/ScrollManager.jsx
@@ -14,61 +14,78 @@ export default function ScrollManager() {
   }, []);
 
   useEffect(() => {
-    const pageKey = `${location.pathname}${location.search}`;
+  const isProductDetailPage = /^\/product\/[^/]+$/.test(location.pathname);
+  const pageKey = isProductDetailPage
+    ? location.pathname
+    : `${location.pathname}${location.search}`;
 
-    const saveScroll = () => {
-      scrollPositions.set(pageKey, window.scrollY || 0);
-    };
+  const saveScroll = () => {
+    scrollPositions.set(pageKey, window.scrollY || 0);
+  };
 
-    window.addEventListener('scroll', saveScroll, { passive: true });
+  window.addEventListener('scroll', saveScroll, { passive: true });
 
-    return () => {
-      saveScroll();
-      window.removeEventListener('scroll', saveScroll);
-    };
-  }, [location.pathname, location.search]);
+  return () => {
+    saveScroll();
+    window.removeEventListener('scroll', saveScroll);
+  };
+}, [location.pathname, location.search]);
 
     useEffect(() => {
-    const pageKey = `${location.pathname}${location.search}`;
+  const isProductDetailPage = /^\/product\/[^/]+$/.test(location.pathname);
+  const pageKey = isProductDetailPage
+    ? location.pathname
+    : `${location.pathname}${location.search}`;
 
-    if (location.pathname === '/') {
+  if (location.pathname === '/') {
+    window.scrollTo(0, 0);
+
+    requestAnimationFrame(() => {
       window.scrollTo(0, 0);
 
       requestAnimationFrame(() => {
         window.scrollTo(0, 0);
 
-        requestAnimationFrame(() => {
+        setTimeout(() => {
           window.scrollTo(0, 0);
-
-          setTimeout(() => {
-            window.scrollTo(0, 0);
-          }, 300);
-        });
+        }, 300);
       });
+    });
 
+    return;
+  }
+
+  if (navigationType === 'POP') {
+    const isMobileProductsPage =
+      location.pathname === '/products' && window.innerWidth <= 768;
+
+    if (isMobileProductsPage) {
       return;
     }
 
-    if (navigationType === 'POP') {
-      const isMobileProductsPage =
-        location.pathname === '/products' && window.innerWidth <= 768;
+    const savedY = scrollPositions.get(pageKey) ?? 0;
+    window.scrollTo(0, savedY);
+    return;
+  }
 
-      if (isMobileProductsPage) {
-        return;
-      }
-
-      const savedY = scrollPositions.get(pageKey) ?? 0;
-      window.scrollTo(0, savedY);
-      return;
-    }
-
-        if (location.state?.fromProductGrid) {
-      window.scrollTo(0, 0);
-      return;
-    }
-
+  if (isProductDetailPage && location.state?.fromProductGrid) {
     window.scrollTo(0, 0);
-  }, [location.pathname, location.search, navigationType, location.state?.fromProductGrid]);
+    return;
+  }
+
+  if (isProductDetailPage) {
+    return;
+  }
+
+  if (location.state?.fromProductGrid) {
+    window.scrollTo(0, 0);
+    return;
+  }
+
+  window.scrollTo(0, 0);
+}, [location.pathname, location.search, navigationType, location.state?.fromProductGrid]);
+
+
 
   return null;
 }
