82be8584e20fb78be3681f83b84c4bb75c647e78
ari <ari@smartinet.co.uk>
2026-04-12 12:46:34 +0100

    update 

--- src/MenuContext.js
+++ src/MenuContext.js
@@ -1,8 +1,2 @@
 import { createContext } from 'react';
-
-export const MenuContext = createContext({
-  isMenuOpen: false,
-  setIsMenuOpen: () => {},
-  showProductsLoader: false,
-  setShowProductsLoader: () => {}
-});
+export const MenuContext = createContext({ isMenuOpen: false, setIsMenuOpen: () => {} });
