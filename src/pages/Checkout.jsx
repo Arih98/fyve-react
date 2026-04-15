e3126e760c5bbcd497c92d73b2cf173ad6f2916c
ari <ari@smartinet.co.uk>
2026-04-15 11:57:48 +0100

    update

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -626,6 +626,9 @@ export default function Checkout() {
 
                   console.log('REVOLUT RESULT', revolutResult)
 
+                  console.log('REVOLUT KEY PREFIX', (revolutResult.merchant_public_key || '').slice(0, 20))
+console.log('REVOLUT TOKEN', revolutResult.revolut_order_token)
+
                   const nextToken = revolutResult.revolut_order_token || ''
                   const nextMerchantKey = revolutResult.merchant_public_key || ''
 
