f8c33e2e6f84cc185cb2730d35db8a1b53e38331
ari <ari@smartinet.co.uk>
2026-04-15 01:03:52 +0100

    update

--- src/api/request.js
+++ src/api/request.js
@@ -1,6 +1,6 @@
-const API_BASE = 'https://fyvelondon.com/wp-json'
+const API_BASE = '/wp-json'
 
-const CART_TOKEN_KEY = 'woo_store_cart_token'
+const CART_TOKEN_KEY = `woo_store_cart_token:${window.location.origin}`
 
 function getStoredCartToken() {
   return localStorage.getItem(CART_TOKEN_KEY) || ''
@@ -10,6 +10,10 @@ function setStoredCartToken(token) {
   if (token) {
     localStorage.setItem(CART_TOKEN_KEY, token)
   }
+}
+
+function clearStoredCartToken() {
+  localStorage.removeItem(CART_TOKEN_KEY)
 }
 
 export async function apiRequest(path, options = {}) {
@@ -33,6 +37,10 @@ export async function apiRequest(path, options = {}) {
   const responseCartToken = res.headers.get('Cart-Token')
   if (responseCartToken) {
     setStoredCartToken(responseCartToken)
+  }
+
+  if (res.status === 401 || res.status === 403) {
+    clearStoredCartToken()
   }
 
   const data = await res.json().catch(() => null)
