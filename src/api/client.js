94acd2c778f11764b68e5cacb1272014e0dbd813
ari <ari@smartinet.co.uk>
2026-04-15 03:01:37 +0100

    update

--- src/api/client.js
+++ src/api/client.js
@@ -2,9 +2,9 @@ const API_BASE = `https://fyvelondon.com/wp-json`
 
 export async function apiFetch(path, options = {}) {
   const response = await fetch(`${API_BASE}${path}`, {
-    credentials: 'include'
+    credentials: 'include',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
       ...(options.headers || {})
     },
     ...options
