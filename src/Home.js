a0efaa6bd22e547de66b0a7e6b4129c8614c6f0d
ari <ari@smartinet.co.uk>
2026-04-19 19:52:05 +0100

    update

--- src/Home.js
+++ src/Home.js
@@ -173,12 +173,12 @@ gsap.set('.london-below', { opacity: 0 })
 
         .addLabel('splitReveal', 'lettersIn+=0.72')
 .to('.hero-side-left', {
-  x: '-8vw',
+  xPercent: -63,
   duration: 0.95 * speed,
   ease: 'power3.inOut'
 }, 'splitReveal')
 .to('.hero-side-right', {
-  x: '8vw',
+  xPercent: 63,
   duration: 0.95 * speed,
   ease: 'power3.inOut'
 }, 'splitReveal')
