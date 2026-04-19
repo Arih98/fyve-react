6217946448341e22c9baf2f8c086a3e66cf05858
ari <ari@smartinet.co.uk>
2026-04-19 19:21:09 +0100

    update

--- src/Home.js
+++ src/Home.js
@@ -174,13 +174,13 @@ gsap.set('.london-below', { opacity: 0 })
         .addLabel('splitReveal', 'lettersIn+=0.72')
 .to('.hero-side-left', {
   xPercent: -63,
-  duration: 1.05 * speed,
-  ease: 'power2.inOut'
+  duration: 0.8 * speed,
+  ease: 'expo.inOut'
 }, 'splitReveal')
 .to('.hero-side-right', {
   xPercent: 63,
-  duration: 1.05 * speed,
-  ease: 'power2.inOut'
+  duration: 0.8 * speed,
+  ease: 'expo.inOut'
 }, 'splitReveal')
 .to('.fyve-image-slot', {
   width: intermediateWidth,
