af10a91c529c77de6140fc43d9fc9782755e71c5
ari <ari@smartinet.co.uk>
2026-04-25 23:23:07 +0100

    update

--- src/Header.jsx
+++ src/Header.jsx
@@ -44,7 +44,7 @@ const showBurgerCartBadge =
   isMobile &&
   !isMenuOpen &&
   displayedBagQuantity > 0 &&
-  (isCartPage || isCheckoutPage);
+  (isCartPage || isCheckoutPage || isProductDetailPage);
 
 const totalBagQuantityRef = useRef(totalBagQuantity);
 const isCartAddAnimatingRef = useRef(false);
