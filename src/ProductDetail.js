2e79f2f52822f98f2ed9fdb7c90e015d9bc0d5a2
ari <ari@smartinet.co.uk>
2026-04-26 13:30:11 +0100

    update

--- src/ProductDetail.js
+++ src/ProductDetail.js
@@ -531,7 +531,7 @@ return true;
 
 const currentTotalPrice = Number(current?.price?.current ?? product?.price?.current ?? current?.price ?? product?.price ?? 0).toFixed(2)
 const addToCartLabel = isOutOfStock ? 'Out of Stock' : 'Add to Cart';
-const pdpMobileButtonLabel = isOutOfStock ? 'Out of Stock' : 'Add to Bag';
+const pdpMobileButtonLabel = isOutOfStock ? 'Out of Stock' : 'Add to bag';
 
 useEffect(() => {
   window.dispatchEvent(
