918228f6a423cb41a8a65bf8367c8929bb176048
ari <ari@smartinet.co.uk>
2026-04-29 10:17:14 +0100

    update

--- src/api/request.js
+++ src/api/request.js
@@ -1,6 +1,7 @@
 const API_BASE = `https://fyvelondon.com/wp-json`
 
 const CART_TOKEN_KEY = 'woo_store_cart_token'
+const NONCE_KEY = 'woo_store_nonce'
 
 function getStoredCartToken() {
   return localStorage.getItem(CART_TOKEN_KEY) || ''
@@ -12,23 +13,42 @@ function setStoredCartToken(token) {
   }
 }
 
+function getStoredNonce() {
+  return localStorage.getItem(NONCE_KEY) || ''
+}
+
+function setStoredNonce(nonce) {
+  if (nonce) {
+    localStorage.setItem(NONCE_KEY, nonce)
+  }
+}
+
 export function clearStoredCartToken() {
   localStorage.removeItem(CART_TOKEN_KEY)
+  localStorage.removeItem(NONCE_KEY)
 }
 
 export async function apiRequest(path, options = {}) {
   const headers = new Headers(options.headers || {})
-  const hasBody = options.body !== undefined && options.body !== null
   const method = (options.method || 'GET').toUpperCase()
-  const storedCartToken = getStoredCartToken()
+  const hasBody = options.body !== undefined && options.body !== null
   const isStoreApiRequest = path.startsWith('/wc/store/')
 
-  if (hasBody && !headers.has('Content-Type')) {
+  if (hasBody && !headers.has('Content-Type') && !(options.body instanceof URLSearchParams)) {
     headers.set('Content-Type', 'application/json')
   }
 
-  if (storedCartToken && isStoreApiRequest && method !== 'GET') {
-    headers.set('Cart-Token', storedCartToken)
+  if (isStoreApiRequest && method !== 'GET') {
+    const storedNonce = getStoredNonce()
+    const storedCartToken = getStoredCartToken()
+
+    if (storedNonce && !headers.has('Nonce')) {
+      headers.set('Nonce', storedNonce)
+    }
+
+    if (storedCartToken && !headers.has('Cart-Token')) {
+      headers.set('Cart-Token', storedCartToken)
+    }
   }
 
   const res = await fetch(`${API_BASE}${path}`, {
@@ -38,8 +58,14 @@ export async function apiRequest(path, options = {}) {
   })
 
   const responseCartToken = res.headers.get('Cart-Token')
+  const responseNonce = res.headers.get('Nonce')
+
   if (responseCartToken) {
     setStoredCartToken(responseCartToken)
+  }
+
+  if (responseNonce) {
+    setStoredNonce(responseNonce)
   }
 
   const data = await res.json().catch(() => null)
