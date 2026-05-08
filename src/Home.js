b7d93ad388eb8b726283b089bee635a408dcfec5
ari <ari@smartinet.co.uk>
2026-05-08 14:04:59 +0100

    update

--- src/Home.js
+++ src/Home.js
@@ -369,7 +369,7 @@ gsap.fromTo('.section1-title-line--bottom', {
   })
 })
 
-const titleTravel = isMobile ? 210 : 185
+const titleTravel = isMobile ? 260 : 230
 
 gsap.fromTo('.results-before-after-title', {
   y: isMobile ? '-22vh' : '-18vh'
